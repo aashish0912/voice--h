8a0d5f365c3511f1ee51de13a59fc2918cdc55c2
aashish0912 <aaashish043@gmail.com>
2026-02-10 22:32:08 +0530

    Add cloud deployment support (dynamic PORT)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Store leads in memory (for demo purposes)
 let leads = [];
